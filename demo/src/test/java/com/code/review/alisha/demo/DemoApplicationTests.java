a56a77bdbc7529de41758ad24328fd36a01afceb
alisha aleem <aanjum1@hawk.iit.edu>
2019-11-23 11:04:48 -0600

    spring initializer

---
+++ demo/src/test/java/com/code/review/alisha/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.review.alisha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
